a71418ea24e0f3633d7e7554fc906a24860e4a62
Pozharov Roman <ru.roman.ps@gmail.com>
2020-05-06 06:06:27 +0400

    Clean JS

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,34 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+const CALLBACK = document.querySelector(".header__callback");
+const POPUP_DONE = document.querySelector(".popup--done");
+const POPUP_REQUEST = document.querySelector(".popup--request");
+const POPUP_CLOSE = document.querySelector(".popup__close");
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+function openPopupRequest(evt) {
+  evt.preventDefault();
+  if (POPUP_REQUEST.classList.contains("hidden")) {
+    POPUP_REQUEST.classList.remove("hidden");
+    document.body.style.opacity = "0.95";
+    document.body.style.background = "#EBEBEB";
   }
-});
+}
+
+function hidden(evt) {
+  evt.preventDefault();
+  evt.currentTarget.classList.toggle("hidden");
+}
+
+function closePopup(evt) {
+  evt.preventDefault();
+  let child = evt.target;
+  let parent = evt.currentTarget;
+  console.log(child);
+  console.log(parent);
+  /* if (child != parent) {
+    if (child.classList.contains)
+  } */
+}
+
+CALLBACK.addEventListener("click", openPopupRequest);
+POPUP_REQUEST.addEventListener("click", closePopup);
